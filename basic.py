ee8f553378065ab7ad9a370984f25fdbd40530c0
AcquirecomeS <hgokul386@gmail.com>
2023-05-23 14:51:39 +0530

    python variables

---
+++ basic.py
@@ -0,0 +1,20 @@
+print('Enter your age',)
+inputAge=input()
+print("Enter the name :")  
+inputName=input()
+print("enter your 10th mark")
+inputMark=input()
+print("your name is "+ inputName +" and age is "+ inputAge+ " you have passed ")
+print("enter your 12th marks")
+m1=input()
+m2=input()
+m3=input()
+m4=input()
+m5=input()
+
+print(""" tamil =""",m1,"""
+math =""",m2,"""
+english =""",m3,"""
+science =""",m4,"""
+social =""",m5,"""
+""")
